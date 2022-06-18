1e0cf090e7df5fa4c68acb07ee9d24102e3eb56c
小克 <goodjack@users.noreply.github.com>
2022-06-18 23:20:18 +0800

    feat: Adopt good coding practices

--- Main.kt
+++ Main.kt
@@ -1,7 +1,6 @@
 fun main() {
     val myFirstDice = Dice(6)
-    val diceRoll = myFirstDice.roll()
-    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
 
     val mySecondDice = Dice(20)
     println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
@@ -9,7 +8,6 @@ fun main() {
 
 class Dice (val numSides: Int) {
     fun roll(): Int {
-        val randomNumber = (1..numSides).random()
-        return randomNumber
+        return (1..numSides).random()
     }
 }
