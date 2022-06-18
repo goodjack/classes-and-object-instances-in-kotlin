d8ab23bf26bfb945e3bfcb87c11cb8b9aaa474dd
小克 <goodjack@users.noreply.github.com>
2022-06-18 22:50:42 +0800

    feat: Create a Dice class

--- Main.kt
+++ Main.kt
@@ -1,5 +1,14 @@
 fun main() {
-    val diceRange = 1..6
-    val randomNumber = diceRange.random()
-    println("Random number: ${randomNumber}")
+    val myFirstDice = Dice()
+    println(myFirstDice.sides)
+    myFirstDice.roll()
+}
+
+class Dice {
+    var sides = 6
+
+    fun roll() {
+        val randomNumber = (1..6).random()
+        println(randomNumber)
+    }
 }
